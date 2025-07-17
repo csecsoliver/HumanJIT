d144cf3683b585dade0c1ef4cedffa3efa6078c2
csecsoliver <0oliogamer0@gmail.com>
2025-07-17 22:37:53 +0200

    sometimes it works

--- backend/channel.ts
+++ backend/channel.ts
@@ -13,24 +13,28 @@ export class Channel {
     if (this.players.length >= 2) {
       throw new Error("Channel is full");
     }
+
     const player = {
       socket: socket,
-      num: (this.players.length + 1).toString(),
-      role: this.players.length === 0 ? "coder" : "compiler" as "coder" | "compiler",
+      num: this.players[0]?.num == "1"? "2": "1",
+      role:
+        this.players[0]?.role == "compiler"
+          ? "coder"
+          : "compiler" as "coder" | "compiler",
     };
     this.players.push(player);
     socket.emit("joined", this.name);
     socket.emit("role", player.role);
     socket.emit("num", player.num);
     socket.join(this.name);
-    socket.on("disconnect", () => {
-      console.log("user disconnected");
+    socket.on("disconnect", (reason) => {
+      console.log("user disconnected: "+ reason);
       this.disconnectPlayer(player);
     });
     socket.on("state", () => {
       this.status(player);
     });
-    
+
     socket.on("line", (line: string) => {
       console.log(line);
       this.codeLines.push(line);
@@ -40,37 +44,44 @@ export class Channel {
         }
       }
       socket.emit("code", this.codeLines);
-
     });
-    socket.on("ack", ()=>{
-        for (const p of this.players) {
+    socket.on("ack", () => {
+      for (const p of this.players) {
         if (p.socket.id !== socket.id) {
           p.socket.emit("ack");
         }
       }
+    });
+    socket.on("line:get", ()=>{
+      player.socket.emit("line:get", this.codeLines[-1]);
     })
-
+    
   }
 
   status(player: Player) {
-    console.log(player);
+    
     player.socket.emit("state", {
       num: player.num,
       role: player.role,
       players: this.players.length,
+      
     });
   }
   disconnectPlayer(player: Player) {
-    const index = this.players.findIndex((p) => p.socket.id === player.socket.id);
+    const index = this.players.findIndex(
+      (p) => p.socket.id === player.socket.id
+    );
+    console.log(`index ${index} is being spliced out`)
     if (index !== -1) {
       this.players.splice(index, 1);
-      
+
       player.socket.leave(this.name);
       if (this.players.length === 0) {
         delete channels[this.name];
       }
       player.socket.disconnect(true);
     }
+    console.log(`index ${index} was being spliced out: ${this.players}`)
   }
 }
 interface Player {
--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,37 +2,50 @@
 import { ref } from 'vue'
 import BackendStatus from './components/BackendStatus.vue'
 import KnownServers from './components/KnownServers.vue'
-import { io } from "socket.io-client";
-import { useSocketStore } from './stores/socket';
-const role = ref("")
-const players = ref(0);
-const line = ref("");
+import { io } from 'socket.io-client'
+import { useSocketStore } from './stores/socket'
+import type { RefSymbol } from '@vue/reactivity'
+const role = ref('')
+const players = ref(0)
+const line = ref('')
 
-const code = ref("");
-const thinking = ref(false);
+const code = ref('')
+const thinking = ref(false)
 async function refresh() {
-  useSocketStore().socket?.once("state", (arg)=>{
-    role.value = arg.role;
-    players.value = arg.players;
+  useSocketStore().socket?.once('state', (arg) => {
+    role.value = arg.role
+    players.value = arg.players
   })
-  useSocketStore().socket?.emit("state")
-}
-async function submitLine() {
-  thinking.value = true;
-  line.value = "";
-  useSocketStore().socket?.once("code", (arg) => {
-    code.value = ""
-    for (const i of arg as string[]){
-      code.value = code.value + i + "\n"
+  useSocketStore().socket?.emit('state')
+  useSocketStore().socket?.off('line')
+  useSocketStore().socket?.on('line', (arg) => {
+    console.log(arg);
+    if (role.value == 'compiler') {
+      line.value = arg;
+      thinking.value = false;
     }
   })
-  useSocketStore().socket?.emit("line", line.value);
-  useSocketStore().socket?.once("ack", ()=>{
-    thinking.value = false;
-    line.value = "";
+}
+async function submitLine() {
+  thinking.value = true
+  useSocketStore().socket?.once('code', (arg) => {
+    code.value = ''
+    for (const i of arg as string[]) {
+      code.value = code.value + i + '\n'
+    }
+  })
+  console.log(line.value)
+  useSocketStore().socket?.emit('line', line.value)
+  line.value = ''
+  useSocketStore().socket?.once('ack', () => {
+    thinking.value = false
+    line.value = ''
   })
 }
-setInterval(refresh, 1000)
+async function ack() {
+  thinking.value = true
+}
+setInterval(refresh, 5000)
 </script>
 
 <template>
@@ -41,28 +54,51 @@ setInterval(refresh, 1000)
     <BackendStatus />
   </header>
   <main>
-    <p>Hello, and welcome to the coding practice game that's every vibecoder's nightmare. You will be playing against one of your friends, and challenging their coding skills</p>
+    <p>
+      Hello, and welcome to the coding practice game that's every vibecoder's nightmare. You will be
+      playing against one of your friends, and challenging their coding skills
+    </p>
 
-    <p>The game has no real cheating prevention/request security, and in case you would want to play illegitimately, just don't even bother.</p>
+    <p>
+      The game has no real cheating prevention/request security, and in case you would want to play
+      illegitimately, just don't even bother. Additionally, connection drops are not necessarily
+      handled.
+    </p>
+    <hr />
     <div v-if="role == 'coder'">
-      <p>You are the coder, your partner can only get see your newest line after you send it. You can only submit a new line once your partner has acknowledged your previous one.</p>
-      <label for="code">Your existing code</label>
+      <p>
+        You are the coder, your partner can only get to see your newest line after you send it. You
+        can only submit a new line once your partner has acknowledged your previous one.
+      </p>
+      <hr />
+      <label for="code">Your existing code</label><br />
       <textarea name="code" id="code" v-model="code" readonly></textarea>
       <form @submit.prevent="submitLine">
-        <label for="line">Your next line: </label>
-        <input type="text" name="line" id="line" v-model="line" :readonly="thinking">
+        <label for="line">Your next line: </label><br />
+        <input type="text" name="line" id="line" v-model="line" :readonly="thinking" />
         <button type="submit" :disabled="thinking">Submit line</button>
       </form>
-      <p>To finish writing your code, and compare with your partner's results, press the button below.</p>
+      <p>
+        To finish writing your code, and compare with your partner's results, press the button
+        below.
+      </p>
       <button @click="">Finish</button>
     </div>
     <div v-else-if="role == 'compiler'">
-      <p>You are the compiler, you are given a line of code every turn and you must keep track of the code in your notes field.</p>
-      <p>You are not supposed to copy/save the lines you reveive, but take notes of the variables, function purposes, etc. in everyday language or just your own symbols</p>
-      <p v-if="line == ''"></p>
-      <input type="text" readonly v-model="line">
-
-
+      <p>
+        You are the compiler, you are given a line of code every turn and you must keep track of the
+        code in your notes field.
+      </p>
+      <p>
+        You are not supposed to copy/save the lines you reveive, but take notes of the variables,
+        function purposes, etc. in everyday language or just your own symbols
+      </p>
+      <hr />
+      <p v-if="line == ''">Wait for the coder to make the first move</p>
+      <input type="text" readonly v-model="line" />
+      <button :disabled="thinking" @click="ack">Understood</button><br />
+      <p>Write your notes below, feel free to resize the textbox</p>
+      <textarea name="notes" id="notes"></textarea>
     </div>
   </main>
 </template>
