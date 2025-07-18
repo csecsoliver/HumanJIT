204d50707be4418daf4031fe765b4edaab257870
csecsoliver <csecs.oliver.elemer@students.jedlik.eu>
2025-07-18 03:08:26 +0200

    minor adjustment

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # HumanJIT
 A coding practice app/game. One person codes, the other interprets.
+
+# Inner workings
--- install.sh
+++ install.sh
@@ -2,8 +2,6 @@
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
 \. "$HOME/.nvm/nvm.sh"
 nvm install 22
-git clone https://github.com/csecsoliver/HumanJIT
-cd HumanJIT
 cd backend
 npm i
 npx tsx index.ts
